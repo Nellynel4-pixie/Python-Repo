e5adc8e15fca7927ed47ce9584decf85c8d65627
Trezah <trezahn44@gmail.com>
2025-08-04 17:05:50 +0300

    Add files via upload

---
+++ trezah.py
@@ -0,0 +1,24 @@
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operator == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operator == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation! Please enter one of +, -, *, /.")
+
+print("-" * 30)
+
